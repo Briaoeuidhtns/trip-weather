edbaebcc570574a602e2be0a14652774d88774aa
Brian Brunner <brian.brunner.bb@gmail.com>
2026-06-20 22:52:00 -0600

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     allowedHosts: ['brian-desktop-nixos.tail106e8.ts.net'],
